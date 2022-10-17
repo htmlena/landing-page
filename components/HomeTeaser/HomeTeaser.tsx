6be45cd4bf61aee6de877bf7c5cd3c04f37f295c
Lenka Shynkarova <lllee@post.cz>
2022-10-17 11:08:13 +0200

    button click area expanded

--- components/HomeTeaser/HomeTeaser.tsx
+++ components/HomeTeaser/HomeTeaser.tsx
@@ -4,26 +4,40 @@ import mainPhoto from "../../public/main-photo.jpg";
 
 const HomeTeaser = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-between items-center gap-10 mt-14 md:mt-20">
-      <div className="md:max-w-[50%]">
-        <h1 className="text-3xl">Lenka Shynkarova</h1>
-        <p className="mt-6 max-w-md">
-          I am a curious frontend developer with over 1 year of commercial
-          experience for an e-commerce platform. I'm looking for motivating work
-          environment and I always strive to create a great web experience,
-          learn something new and contribute to the team success.
-        </p>
-        <button
-          type="button"
-          className="text-white bg-gradient-to-r from-main-light to-main-dark hover:bg-gradient-to-bl font-medium rounded-md text-sm mt-6 px-10 py-2 md:py-1.5 text-center"
-        >
-          <Link href="/about">Learn more</Link>
-        </button>
+    <>
+      <div className="flex flex-col md:flex-row justify-between items-center gap-10 mt-14 md:mt-20">
+        <div className="md:max-w-[50%]">
+          <h1 className="text-3xl">Lenka Shynkarova</h1>
+          <p className="mt-6 max-w-md">
+            I am a curious frontend developer with over 1 year of commercial
+            experience for an e-commerce platform. I'm looking for motivating
+            work environment and I always strive to create a great web
+            experience, learn something new and contribute to the team success.
+          </p>
+
+          <Link href="/about">
+            <button
+              type="button"
+              className="text-white bg-gradient-to-r from-main-light to-main-dark hover:bg-gradient-to-bl font-medium rounded-md text-sm mt-6 px-10 py-2 md:py-1.5 text-center"
+            >
+              Learn more
+            </button>
+          </Link>
+        </div>
+        <div className="w-60 h-auto md:max-w-[50%]">
+          <Image
+            src={mainPhoto}
+            alt="Picture of the author"
+            className="w-full"
+          />
+        </div>
       </div>
-      <div className="w-60 h-auto md:max-w-[50%]">
-        <Image src={mainPhoto} alt="Picture of the author" className="w-full" />
+      <div className="flex flex-col mt-14 md:mt-20">
+        <div>
+          <h2 className="text-3xl">What I did</h2>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
